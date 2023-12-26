f8de99bd64c220311c84d2838b278a74f63cd766
liyanjun <kafka673@gmail.com>
2023-12-26 14:25:26 +0800

    feature -> kv local storage

--- lib/global.dart
+++ lib/global.dart
@@ -6,10 +6,11 @@ import 'common/index.dart';
 class Global {
   static Future<void> init() async {
     WidgetsFlutterBinding.ensureInitialized();
-
+    // 工具类-存储
+    await Storage().init();
+    // 工具类-版本
     await Future.wait([
       Get.putAsync<ConfigService>(() async => await ConfigService().init()),
-    ]).whenComplete(() {
-    });
+    ]).whenComplete(() {});
   }
 }
