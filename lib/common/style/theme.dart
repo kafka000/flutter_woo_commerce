6fd124436c860c25ac9205333df556a9a30e0f09
liyanjun <kafka673@gmail.com>
2023-12-27 18:23:07 +0800

    feature -> 导入字体

--- lib/common/style/theme.dart
+++ lib/common/style/theme.dart
@@ -7,10 +7,12 @@ class AppTheme {
   /// 亮色
   static ThemeData light = ThemeData(
     colorScheme: lightColorScheme,
+    fontFamily: "Roboto",
   );
 
   /// 暗色
   static ThemeData dark = ThemeData(
     colorScheme: darkColorScheme,
+    fontFamily: "Roboto",
   );
 }
